e0de562d9e096b8f94a163ccf60271a3471f0a02
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2025-06-05 16:18:26 +0200

    fix map height

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,8 +1,7 @@
 <template>
-  <div style="width: 100%; height: 100%;">
+  <div class="map">
     <mapbox-map
-      ref="mapboxmap"
-      access-token=""
+      access-token="pk.eyJ1IjoiY29hc3R2aWV3ZXIiLCJhIjoiY2szNW9ncXVsMGtzNzNocGVxZDA5cHlzNiJ9.m8m9xVdtdxvZXWn30rNBQw"
       :center="[4.7, 52.2]"
       class="map"
       map-style="mapbox://styles/mapbox/light-v11"
@@ -14,3 +13,9 @@
 <script setup>
   import { MapboxMap } from '@studiometa/vue-mapbox-gl'
 </script>
+<style>
+.mapboxgl-map, .map {
+  width: 100%;
+  height: 100%;
+}
+</style>
