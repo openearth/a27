4de1ac37f4a99952e873034d445a396971d7d3a8
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2025-06-05 11:36:04 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>My App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app permanent>
+      <!-- Navigation items -->
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: true,
+      }
+    },
+  }
+</script>
