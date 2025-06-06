8a873d5a1a6e90fa88b92fda91a49223924d8c09
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2025-06-06 14:07:53 +0200

    panel in the bottom

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,9 @@
       <v-toolbar-title>a27</v-toolbar-title>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app permanent />
-
     <v-main>
       <map-component />
+      <div class="app-panel" />
     </v-main>
   </v-app>
 </template>
@@ -20,3 +19,17 @@
     },
   }
 </script>
+
+<style scoped>
+
+.app-panel {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 66vh;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0 -2px 8px 0px rgba(0, 0, 0, .3);
+}
+
+</style>
