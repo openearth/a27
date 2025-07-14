df9df90fe389bcccfb94ecf371e83a5c0bda8ff4
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2025-07-14 14:53:02 +0200

    add configurable post requests to wps

--- src/lib/get-timeseries-data.js
+++ src/lib/get-timeseries-data.js
@@ -1,46 +1,30 @@
-import requestUrl from './request-url'
+import sendWpsRequest from '@/lib/wps'
 
-export default async function getTimeseriesData (peilfilterId) {
+export default async function getTimeseriesData (peilfilterId, startDate = '', endDate = '') {
   if (!peilfilterId) {
     throw new Error('No peilfilterId provided to getTimeseriesData')
   }
-  const peilfilterinfo = JSON.stringify({
+  const peilfilterinfo = {
     peilfilterid: peilfilterId,
-    start_date: '',
-    end_date: '',
-  })
-
-  const url = requestUrl({
-    url: import.meta.env.VITE_APP_BASE_URL + '/wps',
-    request: 'Execute',
-    service: 'wps',
-    version: '2.0.0',
-    Identifier: 'wps_get_peilfilter_data',
-    datainputs: `peilfilterinfo=${peilfilterinfo}`,
-  })
-
-  return fetch(url)
-    .then(response => response.text())
-    .then(string => {
-      const document = new window.DOMParser().parseFromString(string, 'text/xml')
-
-      const method = 'getElementsByTagName'
-      const element = document[method]('wps:ComplexData')
-
-      if (element.length === 0) {
-        console.warn('No <wps:ComplexData> found. Raw XML:', string)
-        throw new Error('No <wps:ComplexData> element found')
-      }
-      const value = JSON.parse(element[0].textContent)
-
-      if (value.errMsg) {
-        throw new Error(value.errMsg)
-      }
-
-      return value
+    start_date: startDate,
+    end_date: endDate,
+  }
+  try {
+    const response = await sendWpsRequest({
+      identifier: 'wps_get_peilfilter_data',
+      inputs: [{ id: 'peilfilterinfo', title: 'Peilfilterinfo as peilfilterId, StartDate and EndDate',
+        type: 'ComplexData', mimeType: 'application/json', value: peilfilterinfo }],
+      outputIdentifier: 'peilfilter_data',
+      mimeType: 'application/json',
     })
-    .catch(error => {
-      console.error('Failed to fetch timeseries:', error)
-      throw error
-    })
+
+    if (response.errMsg) {
+      throw new Error(response.errMsg)
+    }
+
+    return response
+  } catch (error) {
+    console.error('Failed to fetch locations:', error)
+    throw error
+  }
 }
